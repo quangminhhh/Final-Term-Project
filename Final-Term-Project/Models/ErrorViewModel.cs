1792a1150e7c8bcc662bc0c4111ea7b4078df16f
quangminhhh <9a1minhcl@gmail.com>
2024-11-03 12:44:09 +0700

    Try Github
    First Commit

---
+++ Final-Term-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Final_Term_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
